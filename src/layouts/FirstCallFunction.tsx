469faedaa7da64215fbf815a80aec54c911df584
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-01-30 07:37:01 +0530

    page-loading

--- src/layouts/FirstCallFunction.tsx
+++ src/layouts/FirstCallFunction.tsx
@@ -16,6 +16,7 @@ const FirstCallFunction = ({ children }: ChildProps) => {
 
 	const fetchUser=async ()=>{
 		await dispatch(fetchProfileData())
+		console.log(profile.loading);
 		setLoading(profile.loading)
 	}
 
@@ -23,7 +24,7 @@ const FirstCallFunction = ({ children }: ChildProps) => {
 		changeTheme(theme);
 		fetchUser()
 	}, []);
-	
+
 	return loading ? <PageLoading /> : <>{children}</>;
 };
 

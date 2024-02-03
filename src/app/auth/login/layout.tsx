22d57ddb5cc246181aba822e23420dada4856d0f
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-02-03 07:38:18 +0530

    login-form

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,23 @@
+import { ChildProps } from "@/types/types";
+import Image from "next/image";
+import React from "react";
+
+import bannerImage from "../../../assets/nature-3082832_1920.jpg"
+
+const index = ({ children }: ChildProps) => {
+	return (
+		<div className={`w-full h-full absolute left-0 top-0 z-50`}>
+			<div
+				className={`w-full h-full absolute left-0 top-0`}
+			>
+				<Image src={bannerImage} className="w-full h-full object-cover" alt="signup-banner" />
+			</div>
+			<div
+				className={`w-full h-full absolute left-0 top-0 backdrop-blur bg-slate-950/50`}
+			></div>
+			{children}
+		</div>
+	);
+};
+
+export default index;
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useState } from "react";
+import { SignupType } from "@/types/types";
+import InputComponent from "@/components/FormComponent/InputComponent";
+
+import { FaGithub } from "react-icons/fa6";
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+
+const index = () => {
+	const [formState, setFormState] = useState<SignupType>({
+		username: "",
+		email: "",
+		password: "",
+	});
+
+	return (
+		<div
+			className={`w-full h-full flex justify-center items-center z-50 relative`}
+		>
+			<div
+				className={`container w-96 h-auto p-5 rounded bg-slate-950 flex justify-start items-center flex-col`}
+			>
+				<h1
+					className={`w-full h-auto text-slate-300 font-bold text-2xl pb-5`}
+				>
+					Login
+				</h1>
+				<form
+					className={`w-full h-auto flex justify-start items-start gap-4 text-slate-300 flex-col`}
+					onSubmit={(event: any) => {
+						event.preventDefault();
+					}}
+				>
+					<InputComponent
+						label="email"
+						required={true}
+						type="email"
+					/>
+					<InputComponent
+						label="password"
+						required={true}
+						type="password"
+					/>
+
+					<div
+						className={`w-full h-auto flex justify-end items-end gap-2 text-xs`}
+					>
+						Create a new Account?{" "}
+						<Link href={"/auth/signup"} className="text-highlight font-bold">
+							Signup
+						</Link>
+					</div>
+
+					<button
+						type="submit"
+						className={`w-full h-10 bg-slate-200 text-slate-900 font-bold rounded`}
+					>
+						Login
+					</button>
+				</form>
+				<div
+					className={`w-[80%] flex justify-center items-center gap-1 my-4`}
+				>
+					<span className={`flex-1 h-[2px] bg-border`}></span>
+					<span className={` text-primary-fg`}>OR</span>
+					<span
+						className={`flex-1 h-[2px] bg-border font-bold`}
+					></span>
+				</div>
+				<div
+					className={`w-full h-auto flex justify-between items-center gap-2`}
+				>
+					<button
+						className={`w-full h-auto py-2 rounded flex justify-center items-center gap-2 bg-primary-fg text-primary-bg`}
+						onClick={() => {
+							signIn("github", { callbackUrl: "/" });
+						}}
+					>
+						<FaGithub className={`w-5 h-5`} />
+						<span className={`text-sm font-bold text-primary-bg`}>
+							Login With Github
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default index;

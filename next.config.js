8bacd7e80607609e59160a8091cf9e95642e7b77
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-02-04 20:22:34 +0530

    adding-next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
 	images: {
 		domains: ["firebasestorage.googleapis.com"],
 	},
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
 };
 
 module.exports = nextConfig;

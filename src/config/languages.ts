07a88574feeac1a4a8cdba97fc94e3149740c2a4
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-02-25 19:34:25 +0530

    fixing language issue

--- src/config/languages.ts
+++
@@ -1,7 +0,0 @@
-import english from "../dictionary/en.json";
-import french from "../dictionary/fr.json";
-
-export const languages:any = {
-	en:import("../dictionary/en.json").then((res)=>res),
-	fr:import("../dictionary/fr.json").then((res)=>res)
-}
